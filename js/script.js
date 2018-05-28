643fd1190cd1c90b443ddc172bc2e341bcdb13f2
Konrad Ryczek <konradryczek92@gmail.com>
2018-05-28 17:46:58 +0200

    update script.js content

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 var height = prompt('Podaj wysokość choinki');
 function drawTree(h){	
-	for (i = 1 ; i <= h ; i++){
-		var star = new String();
-		for (k = i ; k <= h - 1; k++){
+	for (var i = 1 ; i <= h ; i++){
+		var star = "";
+		for (var k = i ; k <= h - 1; k++){
 			star += ' ';
 		}
-		for (j = 1 ; j <= 2 * i - 1 ; j++){
+		for (var j = 1 ; j <= 2 * i - 1 ; j++){
 			star += '*';
 		}
 		console.log(star);
